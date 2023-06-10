a2927390e06f52afae8eb58d5b54412ec7c74376
bluepeng9 <bluepeng9@gmail.com>
2023-06-10 21:29:10 +0900

    test: Add test code for apply transaction to service and repository

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -67,4 +67,20 @@ class MemberServiceTest {
         assertTrue(logRepository.find(username).isPresent());
     }
 
+    /**
+     * memberService    @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
+        //when
+        memberService.joinV1(username);
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
 }
